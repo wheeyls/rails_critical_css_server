66cecc9767c4f09a52b4199e8b09d047d1509d5d
Josh Sharpe <josh.m.sharpe@gmail.com>
2017-08-15 11:36:20 -0400

    fix ErrorHandler to properly use Callable

--- lib/rails_critical_css_server/error_handler.rb
+++ lib/rails_critical_css_server/error_handler.rb
@@ -1,9 +1,15 @@
 module RailsCriticalCssServer
   class ErrorHandler
     extend Callable
+    attr_reader :error
 
-    def call(error)
+    def initialize(error)
+      @error = error
+    end
+
+    def call
       Rails.logger.error error
     end
   end
 end
+
