cfdf7be13e4599407ec08f4ef17610c2fcc9e64a
Michael Wheeler <mwheeler@g2crowd.com>
2019-08-05 08:26:10 -0500

    Properly set up error handler

--- lib/rails_critical_css_server/error_handler.rb
+++ lib/rails_critical_css_server/error_handler.rb
@@ -1,8 +1,13 @@
 module RailsCriticalCssServer
   class ErrorHandler
     extend Callable
+    attr_reader :error
 
-    def call(error)
+    def initialize(error)
+      @error = error
+    end
+
+    def call
       Rails.logger.error error
     end
   end
